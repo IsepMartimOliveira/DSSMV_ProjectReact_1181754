5a28e671ac51e6545cf7772feba4628f8fa9288e
IsepMartimOliveira <1181754@isep.ipp.pt>
2023-12-06 14:52:49 +0000

    TabScreen unfineshed

--- OnlineSupertmarket/routes/Routes.js
+++ OnlineSupertmarket/routes/Routes.js
@@ -1,56 +1,14 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import WelcomeScreen from '../screens/WelcomeScreen';
 import RegisterScreen from '../screens/RegisterScreen';
 import LoginScreen from '../screens/LoginScreen';
-import HomePageScreen from '../screens/HomePageScreen';
-import ShoopScreen from '../screens/ShoopScreen';
-import RecipeScreen from '../screens/RecipeScreen';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import TabNavigator from './TabNavigator';
 
 const Stack = createStackNavigator();
-const Tab = createBottomTabNavigator();
 
-const CreateTabNavigator = () => {
-  return (
-    <Tab.Navigator>
-      <Tab.Screen
-        name="Recipe"
-        component={RecipeScreen}
-        options={{
-          tabBarLabel: 'Recipe',
-          tabBarIcon: ({color, size}) => (
-            <MaterialCommunityIcons name="book" color={color} size={size} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Home"
-        component={HomePageScreen}
-        options={{
-          tabBarLabel: 'Home',
-          tabBarIcon: ({color, size}) => (
-            <MaterialCommunityIcons name="home" color={color} size={size} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Shoop"
-        component={ShoopScreen}
-        options={{
-          tabBarLabel: 'Shoop',
-          tabBarIcon: ({color, size}) => (
-            <MaterialCommunityIcons name="cart" color={color} size={size} />
-          ),
-        }}
-      />
-    </Tab.Navigator>
-  );
-};
-
-const AuthStack = () => {
+const MainStack = () => {
   return (
     <Stack.Navigator initialRouteName="Welcome">
       <Stack.Screen
@@ -71,18 +29,19 @@ const AuthStack = () => {
       <Stack.Screen
         options={{headerShown: false}}
         name="MainTabs"
-        component={CreateTabNavigator}
+        component={TabNavigator}
       />
     </Stack.Navigator>
   );
 };
 
 const Routes = () => {
+  const userIsAuthenticated = false;
+
   return (
     <NavigationContainer>
-      <AuthStack />
+      {userIsAuthenticated ? <TabNavigator /> : <MainStack />}
     </NavigationContainer>
   );
 };
-
 export default Routes;
---
+++ OnlineSupertmarket/routes/TabNavigator.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import HomePageScreen from '../screens/HomePageScreen';
+import ShoopScreen from '../screens/ShoopScreen';
+import RecipeScreen from '../screens/RecipeScreen';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+
+const Tab = createBottomTabNavigator();
+
+// Define the icons outside the TabNavigator
+const ShoopIcon = ({color, size}) => (
+  <MaterialCommunityIcons name="account" color={color} size={size} />
+);
+
+const HomeIcon = ({color, size}) => (
+  <MaterialCommunityIcons name="menu" color={color} size={size} />
+);
+
+const RecipeIcon = ({color, size}) => (
+  <MaterialCommunityIcons name="book" color={color} size={size} />
+);
+
+const TabNavigator = () => (
+  <Tab.Navigator>
+    <Tab.Screen
+      name="Shoop"
+      component={ShoopScreen}
+      options={{
+        tabBarLabel: 'Shoop',
+        tabBarIcon: ShoopIcon,
+      }}
+    />
+    <Tab.Screen
+      name="Home"
+      component={HomePageScreen}
+      options={{
+        tabBarLabel: 'Home',
+        tabBarIcon: HomeIcon,
+      }}
+    />
+    <Tab.Screen
+      name="Recipe"
+      component={RecipeScreen}
+      options={{
+        tabBarLabel: 'Recipe',
+        tabBarIcon: RecipeIcon,
+        showIcon: true,
+      }}
+    />
+  </Tab.Navigator>
+);
+
+export default TabNavigator;
