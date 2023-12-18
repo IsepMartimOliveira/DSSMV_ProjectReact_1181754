ccb8e64d9c950e62ae8dfe8b7f6207b0aaa1cae4
IsepMartimOliveira <1181754@isep.ipp.pt>
2023-12-18 07:23:10 +0000

    Gives Permision,Changes on shoopscreen

--- OnlineSupertmarket/screens/ShoopScreen.js
+++ OnlineSupertmarket/screens/ShoopScreen.js
@@ -6,20 +6,21 @@ import {deleteItem, getShoppingCart} from '../service/Request';
 import ShoppingCartItem from '../components/ShoopingCartItem';
 import ShoppingCartActions from '../components/ShoppingCartActions';
 
-
 const ShoppingCartScreen = ({route}) => {
   const {userData} = useUser();
   const [names, setNames] = useState([]);
   const [ids, setIds] = useState([]);
   const [costs, setCosts] = useState([]);
   const [totalCost, setTotalCost] = useState(0);
-  const navigation=useNavigation();
+  const navigation = useNavigation();
   const selectedStreet = route.params?.selectedStreet;
   React.useEffect(() => {
     if (selectedStreet) {
       Alert.alert(
         'Checkout',
-        `Adress: ${selectedStreet.street}\nTotal Cost: $${totalCost.toFixed(2)}`,
+        `Adress: ${selectedStreet.street}\nTotal Cost: $${totalCost.toFixed(
+          2,
+        )}`,
         [
           {
             text: 'OK',
@@ -109,7 +110,7 @@ const ShoppingCartScreen = ({route}) => {
   const pressDeleteAll = index => {
     Alert.alert(
       'Delete all Ingredients',
-      'Do you want to delete all itemns?',
+      'Do you want to delete all items?',
       [
         {text: 'Cancel', style: 'cancel'},
         {text: 'Delete', onPress: () => handleDeleteAll(index)},
@@ -121,9 +122,6 @@ const ShoppingCartScreen = ({route}) => {
   const handleCheckOut = () => {
     navigation.navigate('Map');
   };
-
-
-
 
   const handleDeleteAll = async () => {
     const {username, hash} = userData;
@@ -148,25 +146,36 @@ const ShoppingCartScreen = ({route}) => {
   };
 
   return (
-    <View>
+    <View style={styles.container}>
       <Text style={styles.title}>Shopping Cart Screen </Text>
-      <FlatList
-        data={names}
-        keyExtractor={(item, index) => index.toString()}
-        renderItem={({item, index}) => (
-          <ShoppingCartItem
-            name={item}
-            expectedCost={costs[index]}
-            onPress={() => pressDelete(index)}
-          />
-        )}
-      />
-      <Text style={styles.totalCost}>Total Cost: ${totalCost.toFixed(2)}</Text>
 
-      <ShoppingCartActions
-        onDeletePress={() => pressDeleteAll()}
-        onCheckoutPress={() => handleCheckOut()}
-      />
+      {names.length > 0 ? (
+        <>
+          <FlatList
+            data={names}
+            keyExtractor={(item, index) => index.toString()}
+            renderItem={({item, index}) => (
+              <ShoppingCartItem
+                name={item}
+                expectedCost={costs[index]}
+                onPress={() => pressDelete(index)}
+              />
+            )}
+          />
+          <Text style={styles.totalCost}>
+            Total Cost: ${totalCost.toFixed(2)}
+          </Text>
+
+          <ShoppingCartActions
+            onDeletePress={() => pressDeleteAll()}
+            onCheckoutPress={() => handleCheckOut()}
+          />
+        </>
+      ) : (
+        <Text style={styles.noItemsText}>
+          No items in the shopping cart {'\n'} Go search your next meal!
+        </Text>
+      )}
     </View>
   );
 };
@@ -184,6 +193,9 @@ const styles = StyleSheet.create({
   totalCost: {
     textAlign: 'right',
     fontSize: 20,
-  }
+  },
+  noItemsText: {
+    textAlign: 'center',
+  },
 });
 export default ShoppingCartScreen;
